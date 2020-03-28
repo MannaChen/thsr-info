710ae0e52af1009fc94c24d9100b2be1ad856ea9
Katness <sbxew99@gmail.com>
2020-03-29 02:24:46 +0800

    checkTrain

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="app">
+    <h1>高鐵即時資料查詢</h1>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/checkTrain">查詢車次</router-link> |
+      <router-link to="/checkStop">查詢停靠站</router-link> |
+      <router-link to="/checkSeats">查詢座位</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script>
+
+export default {
+  data: () => ({
+  }),
+  created() {
+    this.$store.dispatch('getStations');
+  },
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', 'Microsoft JhengHei',Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  padding: 20px;
+  h1 {
+    margin: 0 0 20px 0;
+  }
 }
+
 #nav {
-  padding: 30px;
+  margin-bottom: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ src/components/checkTrain.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <el-form label-position="left" label-width="70px">
+      <el-form-item required label="起點">
+        <el-select v-model="departureStation" placeholder="请选择">
+          <el-option
+            v-for="item in $store.state.station"
+            :key="item.StationID"
+            :label="item.StationName.Zh_tw"
+            :value="item.StationID">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item required label="終點">
+        <el-select v-model="arrivalStation" placeholder="请选择">
+          <el-option
+            v-for="item in $store.state.station"
+            :key="item.StationID"
+            :label="item.StationName.Zh_tw"
+            :value="item.StationID">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item required label="日期">
+        <el-date-picker
+          :clearable="false"
+          :editable="false"
+          :picker-options="pickerOption"
+          value-format="yyyy-MM-dd"
+          v-model="departureDate"
+          type="date"
+          placeholder="选择發車日期">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item label="出發時間">
+        <el-time-select
+          v-model="departureTime"
+          :editable="false"
+          :clearable="false"
+          :picker-options="{
+            start: startTimeOption,
+            step: '00:30',
+            end: '23:30'
+          }"
+          placeholder="选择时间">
+        </el-time-select>
+      </el-form-item>
+      <el-form-item label="抵達時間">
+        <el-time-select
+          v-model="arrivalTime"
+          :editable="false"
+          :clearable="false"
+          :picker-options="{
+            start: startTimeOption,
+            step: '00:30',
+            end: '23:30'
+          }"
+          placeholder="选择时间">
+        </el-time-select>
+      </el-form-item>
+      <el-button
+        type="primary"
+        :loading="loading"
+        :disabled="btnDisabled"
+        @click="getTrains">
+        查詢車次
+      </el-button>
+    </el-form>
+    <section class="result">
+      <p>共 {{ result.length }} 筆結果</p>
+      <div v-for="item in result" :key="item.DailyTrainInfo.TrainNo">
+        <span>車次：{{ item.DailyTrainInfo.TrainNo }}，</span>
+        <span>出發：{{ item.OriginStopTime.DepartureTime }}，</span>
+        <span>抵達：{{ item.DestinationStopTime.ArrivalTime }}</span>
+        <hr>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    departureStation: '',
+    arrivalStation: '',
+    departureDate: '',
+    departureTime: '00:00',
+    arrivalTime: '23:59',
+    pickerOption: {
+      disabledDate(date) {
+        const now = new Date();
+        // 已過日期不能選
+        return date < now - 86400000 * 1;
+      },
+    },
+    result: [],
+    startTimeOption: '',
+    loading: false,
+  }),
+  methods: {
+    getTrains() {
+      this.loading = true;
+      this.$store.dispatch('getTrains', {
+        departureStation: this.departureStation,
+        arrivalStation: this.arrivalStation,
+        departureDate: this.departureDate,
+      }).then((res) => {
+        this.loading = false;
+        this.result = res.data.sort((a, b) => {
+          if (a.OriginStopTime.DepartureTime < b.OriginStopTime.DepartureTime) return -1;
+          return 1;
+        }).filter(item => this.departureTime < item.OriginStopTime.DepartureTime)
+          .filter(item => this.arrivalTime > item.DestinationStopTime.ArrivalTime);
+      });
+    },
+    prepandZero(number) {
+      if (number > 10) return number;
+      return `0${number}`;
+    },
+  },
+  computed: {
+    btnDisabled() {
+      return !this.departureStation || !this.arrivalStation;
+    },
+  },
+  created() {
+    const d = new Date();
+    this.departureDate = `${d.getFullYear()}-${this.prepandZero(d.getMonth() + 1)}-${this.prepandZero(d.getDate())}`;
+
+    let hour = d.getHours();
+    let min = d.getMinutes();
+    if (min > 30) {
+      min = '0';
+      hour += 1;
+    } else {
+      min = '30';
+    }
+    this.startTimeOption = `${this.prepandZero(hour)}:${this.prepandZero(min)}`;
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.result {
+  font-size: 13px;
+}
+</style>
